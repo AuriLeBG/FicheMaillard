065aae89ec1ff83c8662c1cc42f3b81b2335f058
AuriLeBG <paul.pihem@hotmail.com>
2025-10-01 23:47:47 +0200

    Création activité addresse

--- app/src/main/java/fr/aurilebg/fichemaillard/AdresseActivity.java
+++ app/src/main/java/fr/aurilebg/fichemaillard/AdresseActivity.java
@@ -2,6 +2,8 @@ package fr.aurilebg.fichemaillard;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -23,6 +25,37 @@ public class AdresseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_adresse);
+
+        Button button = findViewById(R.id.buttonOk);
+
+        Intent intent = getIntent();
+
+        String numero = intent.getStringExtra("fr.aurilebg.fichemaillard.numero");
+        String rue = intent.getStringExtra("fr.aurilebg.fichemaillard.rue");
+        String code_postal = intent.getStringExtra("fr.aurilebg.fichemaillard.code_postal");
+        String ville = intent.getStringExtra("fr.aurilebg.fichemaillard.ville");
+
+        EditText editTextNumero = findViewById(R.id.editTextNumero);
+        EditText editTextRue = findViewById(R.id.editTextRue);
+        EditText editTextCodePostal = findViewById(R.id.editTextCodePostal);
+        EditText editTextVille = findViewById(R.id.editTextVille);
+
+        editTextNumero.setText(numero);
+        editTextRue.setText(rue);
+        editTextCodePostal.setText(code_postal);
+        editTextVille.setText(ville);
+
+        button.setOnClickListener(v->{
+            Intent result = new Intent();
+
+            result.putExtra("fr.aurilebg.fichemaillard.numero", editTextNumero.getText().toString());
+            result.putExtra("fr.aurilebg.fichemaillard.rue", editTextRue.getText().toString());
+            result.putExtra("fr.aurilebg.fichemaillard.code_postal", editTextCodePostal.getText().toString());
+            result.putExtra("fr.aurilebg.fichemaillard.ville", editTextVille.getText().toString());
+            setResult(RESULT_OK, result);
+            finish();
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/fr/aurilebg/fichemaillard/MainActivity.java
+++ app/src/main/java/fr/aurilebg/fichemaillard/MainActivity.java
@@ -19,7 +19,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityResultLauncher<Intent> mGetResult = registerForActivityResult(
+    private ActivityResultLauncher<Intent> mGetResultIdentity = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 @Override
@@ -48,15 +48,44 @@ public class MainActivity extends AppCompatActivity {
             }
     );
 
+    private ActivityResultLauncher<Intent> mGetResultAddress = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == RESULT_OK){
+                        Intent data = result.getData();
+                        assert data != null;
+                        String numero = data.getStringExtra("fr.aurilebg.fichemaillard.numero");
+                        String rue = data.getStringExtra("fr.aurilebg.fichemaillard.rue");
+                        String code_postal = data.getStringExtra("fr.aurilebg.fichemaillard.code_postal");
+                        String ville = data.getStringExtra("fr.aurilebg.fichemaillard.ville");
+
+                        TextView textViewNumero = findViewById(R.id.textViewNumeroUser);
+                        TextView textViewRue = findViewById(R.id.textViewRueUser);
+                        TextView textViewCodePostal = findViewById(R.id.textViewCodePostalUser);
+                        TextView textViewVille = findViewById(R.id.textViewVilleUser);
+
+                        textViewNumero.setText(numero);
+                        textViewRue.setText(rue);
+                        textViewCodePostal.setText(code_postal);
+                        textViewVille.setText(ville);
+                    }
+                }
+            }
+    );
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.buttonIdentity);
+        Button buttonIdentity = findViewById(R.id.buttonIdentity);
+        Button buttonAddress = findViewById(R.id.buttonAddress);
 
-        button.setOnClickListener(v -> {
+        buttonIdentity.setOnClickListener(v -> {
 
             Intent intent = new Intent(MainActivity.this, IndentityActivity.class);
 
@@ -72,9 +101,29 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("fr.aurilebg.fichemaillard.prenom", prenom);
             intent.putExtra("fr.aurilebg.fichemaillard.telephone", telephone);
 
-            mGetResult.launch(intent);
+            mGetResultIdentity.launch(intent);
         });
 
+        buttonAddress.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AdresseActivity.class);
+
+            TextView textViewNumero = findViewById(R.id.textViewNumeroUser);
+            TextView textViewRue = findViewById(R.id.textViewRueUser);
+            TextView textViewCodePostal = findViewById(R.id.textViewCodePostalUser);
+            TextView textViewVille = findViewById(R.id.textViewVilleUser);
+
+            String numero = textViewNumero.getText().toString();
+            String rue = textViewRue.getText().toString();
+            String code_postal = textViewCodePostal.getText().toString();
+            String ville = textViewVille.getText().toString();
+
+            intent.putExtra("fr.aurilebg.fichemaillard.numero", numero);
+            intent.putExtra("fr.aurilebg.fichemaillard.rue", rue);
+            intent.putExtra("fr.aurilebg.fichemaillard.code_postal", code_postal);
+            intent.putExtra("fr.aurilebg.fichemaillard.ville", ville);
+
+            mGetResultAddress.launch(intent);
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
